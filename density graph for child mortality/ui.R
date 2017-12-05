ee2434c05c3b47104e745a06e72b6550ae659aa1
Vineeth Santosh Varghese <vinvsv97@uw.edu>
2017-12-04 22:10:28 -0800

    I1

---
+++ density graph for child mortality/ui.R
@@ -0,0 +1,30 @@
+library("shiny")
+library("dplyr")
+library("ggplot2")
+
+shinyUI(fluidPage(
+  
+  #title
+  titlePanel("Global Child Mortality"),
+  
+  #Layout
+  sidebarLayout(
+    # side panel
+    sidebarPanel(
+      
+      sliderInput('years',                # key this value will be assigned to
+                  "Years",                # label
+                  min = 1800,             # minimum slider value
+                  max = 2015,             # maximum slider value
+                  value = c(1800, 2015)   # starting value
+      )
+    ),
+    
+    #main panel
+    mainPanel(
+      #plotting output
+      plotOutput("rolePlot")
+    )
+  )
+  
+))
