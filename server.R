5e165e6403528a039f9bafb3a36b7f294544821f
Aviral Sharma <aviralsharma98@gmail.com>
2017-12-06 21:40:25 -0800

    added text

--- server.R
+++ server.R
@@ -116,6 +116,11 @@ my.server <- function(input, output) {
     return(statitics)
   })   
   
+  output$diseases <- renderTable({
+    df <- causes.data %>% select(Entity, Year, Deaths) %>% filter(Deaths >= 0)
+    df <- spread(df, Year, Deaths) #converting it from long to wide
+    return(df[1:14, "Entity"])
+  })
   output$table.info <- renderText({
     return("SUMMARY STATISTICS OF CHILD MORTALITY RATE BY GENDER")
   })
@@ -172,6 +177,66 @@ my.server <- function(input, output) {
              yaxis = list(title = "Children per woman",
                           showgrid = FALSE))
     
+  })
+  
+  output$fertility.intro <- renderText({
+    return("This visualization represents the total number of children a woman gave birth to that were victims of child 
+            mortality (died before the age of 5). One can view the contrast regarding different countries, 
+            i.e. the number of children lost by a woman in different countries over time")
+  })
+  
+  output$fertility.summary <- renderText({
+    return("The slider allows the user to select the time period and the drop-down menu lets the user select the country. 
+            The line density plot then displays the comparison between the number of children that survived above the age
+            of 5 (per woman) vs. the number children that did not survive above the age of 5 (per woman). 
+            The Y-axis represents the frequency of children per woman and the X-axis represents the year span")
+  })
+  
+  output$intro <- renderText({
+    return("Child Mortality has been a social issue that has been ever-present in the society. Simply defined, it is the 
+           rate of deaths of children below 5 years of age. Child mortality is dependent on a number of factors varying 
+           from parents' biological wellness to proper health care facilities being provided. As per data provided by 
+           UNICEF, child mortality rates around the world have been on a decline in the recent years. In 19th century 
+           central Europe,  specifically Germany, the child mortality rate was 1 in 2 births whereas in today's 
+           Germany the child mortality rate has significantly come down to 3.7 in 1000 live births. 
+           Even on a global scale, the child mortality rate has reduced to 3.7 million from a massive 7.6 million in the 
+           last 2 decades. A positive development on such a serious social issue has certainly not been highlighted 
+           publically by the media. It is largely due to the fact that the change on global level is a sedate process, 
+           but is it definitely something that's informative and can prove insightful in taking future steps. 
+           This shiny app has been created to primarily achieve the goal of extracting and portraying valuable 
+           information for NGO's and health organizations around the world working towards improving child mortality 
+           rates in different countries. Child Mortality rate data ranging from 1751 to 2015 has been intuitively depicted 
+           through this app which gives an idea of the current state of healthcare facilities andd could consequesntly help
+           take better decisions regarding policies in the future.")
+  })
+  
+  output$causes <- renderText({
+    return("This visualization displays the global freqeuncy of child deaths and their causes in the form of an area-filled
+            graph. The frequency of deaths are on the Y-axis whereas their respective causes are on the X-axis. This visualization
+             uses multiple traces which means that is compares the cases with respect to two cases, i.e. 1990 and 2015
+             For example: The highest deaths recorded in 1990 due to lower respiratory infection were 
+             2.12 million deaths. Correspondingly, the number of deaths recorded due to the same cause in 
+             2015 were 703.9K deaths. We can clearly infer that the child mortality rate was higher in 
+             1990 than 2015. Since the visulaization tells us that more than half of all(51.8% to be precise) 
+             children died due to infectious diseases, we can conclude that over the years there has been an improvement
+              in the availability of vaccinations. ")
+  })
+  
+  output$map.info <- renderText({
+    return("In the time series plot we try to showcase child mortality over the long run. Today child mortality in industrialized 
+           countries is below 0.5%. The time series plot shows that these low mortality rates are a very recent development, 
+           and in pre-modern countries child mortality rates were between 30% and 50%. In the late 19th century, every second 
+           child in Germany died before its fifth birthday. In developing countries the health of children is quickly improving 
+           – but child mortality is still much higher than in developed countries")
+  })
+  
+  output$gender <- renderText({
+    return("This visualization represents the child mortality rate in the form of a line plot. One can select a country
+             and view the child mortality rates of boys or girls or even compare the child mortality rates between
+            genders and different countries. On the Y-axis is the child mortality rate and on the X-axis is the year 
+            ranging from 1990 to 2010. Looking at a fair number of instances, we can infer that in most countries, 
+            over the recent years, the child mortality rates within boys has consistenty been greater than that of girls.
+           ")
   })
   
 }
--- ui.R
+++ ui.R
@@ -23,49 +23,45 @@ colnames(raw.data)[6] <- "Total-Population"
 ui <- navbarPage(
   #Application 
   "Child Mortality Viz",
-  useShinyjs(),
+  
   tabPanel("Introduction",
-           sidebarLayout(
-             # side panel
-             sidebarPanel(
-               
-               sliderInput('introYears',                # key this value will be assigned to
-                           "Years",                # label
-                           min = 1800,             # minimum slider value
-                           max = 2015,             # maximum slider value
-                           sep = "",
-                           value = c(1800, 2015)   # starting value
+           fluidPage(
+             fluidRow(
+               column(12,
+                      mainPanel(
+                                 h1("A visual representation of Child Mortality Rates"),
+                                 br(),
+                                 plotlyOutput("introPlot"),
+                                 sliderInput('introYears',           # key this value will be assigned to
+                                             "Years",                # label
+                                             min = 1800,             # minimum slider value
+                                             max = 2015,             # maximum slider value
+                                             sep = "",
+                                             value = c(1800, 2015)   # starting value
+                                 )
+                                 
+                          )
                )
              ),
-             
-             #main panel
-             mainPanel(
-               #plotting output
-               plotlyOutput("introPlot")
+             br(),
+             textOutput("intro")
              )
-           )
   ),
   tabPanel("Map",
-           fluidPage(
-             # Show a plot of the generated distribution
-             fluidRow(
-               column(12, div(style = "height:200px"),
-                      mainPanel(plotlyOutput("worldmap")),
-                      fluidRow(
-                        column(12, div(style = "height:100px"), 
-                               hr(), 
-                               fluidRow(column(12, sidebarPanel(
-                                 sliderInput("years", "Choose year",
-                                             min = 1751,
-                                             max = 2015,
-                                             sep = "",
-                                             value = 1950,
-                                             animate =
-                                               animationOptions(interval = 300, loop = FALSE))))
-                               )
-                        )
-                      )
-               )
+           fluidRow(
+             column(11,
+                    h2("Our first visualisation depicts the magnitude of decline of Global Child Mortality Rate."),
+                    br(),
+                    textOutput("map.info"),
+                    br(),
+                    mainPanel(plotlyOutput("worldmap")),
+                    sliderInput("years", "Choose year",
+                                min = 1751,
+                                max = 2015,
+                                sep = "",
+                                value = 1950,
+                                animate =
+                                  animationOptions(interval = 300, loop = FALSE))
              )
            )
   ),
@@ -92,12 +88,17 @@ ui <- navbarPage(
              #main panel
              mainPanel(
                #plotting output
-               plotlyOutput("rolePlot")
+               plotlyOutput("rolePlot"),
+               br(),
+               strong(textOutput("fertility.intro")),
+               br(),
+               textOutput("fertility.summary")
              )
            )
   ),
-  
+  useShinyjs(),
   tabPanel("Child mortality by sex",
+           
            sidebarLayout(
              sidebarPanel(
                h2("Please Make Selections"),
@@ -105,15 +106,15 @@ ui <- navbarPage(
                             choices = c("Male" , "Female" , "Both" ), selected = "Male"),
                br(),
                selectInput("ChildCountry", label = h5("Select Country"), 
-                           choices = raw.data$Country, width = '50%',
+                           choices = raw.data$Country, width = '75%',
                            selected = 1),
                
                hidden (
                  selectInput("ChildCountry2", label = h5("Second Country"), 
-                             choices = raw.data$Country, width = '50%',
+                             choices = raw.data$Country, width = '75%',
                              selected = raw.data$Country[4])
                ),
-               actionButton("button", "Add country +", width = '50%'),
+               actionButton("button", "Add country +", width = '75%'),
                width = 2
              ),
              mainPanel(
@@ -123,17 +124,37 @@ ui <- navbarPage(
                br(),
                tableOutput("statitics1"),
                br(),
-               tableOutput("statitics2")
+               tableOutput("statitics2"),
+               br(),
+               textOutput("gender")
              )
            )
   ),
   
   tabPanel("Global child death causes",
+           
+           fluidPage(
            mainPanel(
-             plotlyOutput("areaGraph")
+             fluidRow(
+             column(8,h1("Causes Behind Child Mortality"),
+                    plotlyOutput("areaGraph"),
+                    br(),
+                    textOutput("causes"),
+                    br()
+                    
+             ),
+             fluidRow(
+               column(4, 
+                      br(),
+                      h3("Some of the diseases that cause child mortality:"),
+                      tableOutput("diseases")
+               )
+             )
+           )
            )
   )
 )
+)
 
-#onclick("button", show("infantCountry2"))
+
 
